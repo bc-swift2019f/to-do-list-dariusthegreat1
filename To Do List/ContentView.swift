4d6df9898b5b1b27be9acb776e7f80a89731c93f
Nick Haidari <haidari@bc.edu>
2019-09-22 11:07:13 -0400

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Nick Haidari on 9/22/19.
+//  Copyright © 2019 Nick Haidari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
